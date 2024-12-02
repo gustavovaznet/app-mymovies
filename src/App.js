74eb24cb2f95e66c58d69b89d6174730d8e0d557
Gustavo Vaz <98855318+gustavovaznet@users.noreply.github.com>
2024-12-02 15:25:20 -0300

    Create project files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { Header } from "./components/Header";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { Home } from "./views/Home";
+import { About } from "./views/About";
+import { MovieDetail } from "./views/MovieDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/movie/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
